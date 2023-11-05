abbe9d001f2960dc60813578d8f2bda1f7a10df8
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2023-11-05 10:07:55 +0100

    Adjust sounds volume in mixer.

--- inc/simulator/mixer.h
+++ inc/simulator/mixer.h
@@ -10,18 +10,18 @@
 
 /*** MIXER macros ***/
 
-#define COMPRESSOR_AUDIO_GAIN	0.8
-#define FD_AUDIO_GAIN			1.0
-#define FPB_AUDIO_GAIN			1.0
-#define KVB_AUDIO_GAIN			1.0
-#define MP_AUDIO_GAIN			1.0
-#define MPINV_AUDIO_GAIN		1.0
-#define PBL2_AUDIO_GAIN			1.0
-#define TRACK_AUDIO_GAIN		1.0
-#define ZDJ_AUDIO_GAIN			1.0
-#define ZVM_AUDIO_GAIN			0.7
-#define ZPT_FRONT_AUDIO_GAIN	1.0
-#define ZPT_REAR_AUDIO_GAIN		0.7
-#define WHISTLE_AUDIO_GAIN		1.0
+#define COMPRESSOR_AUDIO_GAIN	0.3
+#define FD_AUDIO_GAIN			0.5
+#define FPB_AUDIO_GAIN			0.5
+#define KVB_AUDIO_GAIN			0.5
+#define MP_AUDIO_GAIN			0.5
+#define MPINV_AUDIO_GAIN		0.5
+#define PBL2_AUDIO_GAIN			0.5
+#define TRACK_AUDIO_GAIN		0.5
+#define ZDJ_AUDIO_GAIN			0.5
+#define ZVM_AUDIO_GAIN			0.2
+#define ZPT_FRONT_AUDIO_GAIN	0.5
+#define ZPT_REAR_AUDIO_GAIN		0.3
+#define WHISTLE_AUDIO_GAIN		0.5
 
 #endif /* __MIXER_H__ */
