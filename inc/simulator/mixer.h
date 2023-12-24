089da5e38aeee215e73fd33a436f9cb8567d92dd
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2023-12-24 11:28:52 +0100

    Add TRACK audio files.

--- inc/simulator/mixer.h
+++ inc/simulator/mixer.h
@@ -13,15 +13,15 @@
 #define COMPRESSOR_AUDIO_GAIN	0.3
 #define FD_AUDIO_GAIN			0.5
 #define FPB_AUDIO_GAIN			0.5
-#define KVB_AUDIO_GAIN			0.5
+#define KVB_AUDIO_GAIN			0.3
 #define MP_AUDIO_GAIN			0.5
 #define MPINV_AUDIO_GAIN		0.5
 #define PBL2_AUDIO_GAIN			0.5
-#define TRACK_AUDIO_GAIN		0.5
+#define TRACK_AUDIO_GAIN		1.0
 #define ZDJ_AUDIO_GAIN			0.5
 #define ZVM_AUDIO_GAIN			0.2
-#define ZPT_FRONT_AUDIO_GAIN	0.5
-#define ZPT_REAR_AUDIO_GAIN		0.3
+#define ZPT_FRONT_AUDIO_GAIN	0.6
+#define ZPT_REAR_AUDIO_GAIN		0.4
 #define WHISTLE_AUDIO_GAIN		0.5
 
 #endif /* __MIXER_H__ */
