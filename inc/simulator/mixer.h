4ff72066094947b8f52514a769cc6f1d6031c9cb
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2024-01-01 13:24:16 +0100

    Reduce keyboard single press duration to improve commands latency.

--- inc/simulator/mixer.h
+++ inc/simulator/mixer.h
@@ -15,7 +15,7 @@
 #define FPB_AUDIO_GAIN			1.0
 #define EMERGENCY_AUDIO_GAIN	0.8
 #define KVB_AUDIO_GAIN			0.3
-#define MP_AUDIO_GAIN			0.2
+#define MP_AUDIO_GAIN			0.3
 #define MPINV_AUDIO_GAIN		0.5
 #define PBL2_AUDIO_GAIN			0.5
 #define TRACK_AUDIO_GAIN		1.0
